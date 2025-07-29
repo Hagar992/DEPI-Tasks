ac3999e2a5645defb6739d1a54aca8714dea0fa4
Shams A <shamsatia96@gmail.com>
2025-07-29 12:56:24 +0300

    First commit

---
+++ C#-Taskes/Task-1(DEPI)/Task-1(DEPI)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      
+        Console.WriteLine("Hello!");
+ 
+        Console.Write("Input the first number: ");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+     
+        Console.Write("Input the second number: ");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("What do you want to do with those numbers?");
+        Console.WriteLine("[A]dd");
+        Console.WriteLine("[S]ubtract");
+        Console.WriteLine("[M]ultiply");
+       
+        string? choice = Console.ReadLine();
+       
+        switch (choice?.ToLower()) 
+        {
+            case "a":
+                Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+                break;
+
+            case "s":
+                Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+                break;
+
+            case "m":
+                Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+                break;
+
+            default:
+                Console.WriteLine("Invalid option");
+                break;
+        }
+
+        Console.WriteLine("Press any key to close");
+        Console.ReadKey();
+    }
+}
