5a1c77eba5e9611ffc85737154f026a6eb5f8b79
Shams A <shamsatia96@gmail.com>
2025-09-14 13:08:22 +0300

    Task CodeFirst

---
+++ Linq/Code first/Code first/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Collections.Generic;
+
+public class MyDbContext : DbContext
+{
+    public DbSet<Catalog> Catalogs { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<News> News { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      
+        optionsBuilder.UseSqlServer(@"Server=.;Database=NewsDB;Trusted_Connection=True;");
+
+        //Add - Migration InitialCreate
+        //Update - Database
+
+    }
+}
---
+++ Linq/Code first/Code first/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Catalog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Desc { get; set; }
+
+    // علاقة 1 - Many مع News
+    public ICollection<News> NewsList { get; set; }
+}
+
+public class Author
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public DateTime JoinDate { get; set; }
+
+    // علاقة 1 - Many مع News
+    public ICollection<News> NewsList { get; set; }
+}
+
+public class News
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Bref { get; set; }
+    public string Desc { get; set; }
+    public string Time { get; set; }
+    public DateTime Date { get; set; }
+
+    // العلاقات
+    public int AuthorId { get; set; }
+    public Author Author { get; set; }
+
+    public int Cat_Id { get; set; }
+    public Catalog Catalog { get; set; }
+}
